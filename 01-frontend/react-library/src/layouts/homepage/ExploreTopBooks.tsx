fae7b178c0acb5bb6eb34385bf33e1cf414b00bf
Santosh Reddy <santu.santosh16@gmail,com>
2024-04-12 20:29:55 -0500

    created Navabr and TopBooks component

---
+++ 01-frontend/react-library/src/layouts/homepage/ExploreTopBooks.tsx
@@ -0,0 +1,15 @@
+export const ExploreTopBooks = () => {
+    return (
+        <div className="p-5 mb-4 bg-dark header">
+            <div className="container-fluid py-5 
+            text-white d-flex justify-content-center 
+            align-item-center">
+                <div>
+                    <h1 className="display-5 fw-bold">Find you next adventure</h1>
+                    <p className="col-md-8 fs-4">Where would you like to go next?</p>
+                    <a href="#" type="button" className="btn main-color btn-lg text-white">Explore top books</a>
+                </div>
+            </div>
+        </div>
+    )
+}
